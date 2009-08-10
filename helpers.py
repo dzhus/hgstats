eb65916819150c49b5ae1d0e95d9e4ab78aaca60
Dmitry Dzhus <dima@sphinx.net.ru>
2009-08-10 15:48:42 +0400

    * hgstats.py (run_stats): Now just prints revision count stats for each repository listed in command line args.
    * helpers.py (StatItem, CtxStatItem, StatStream, RepoStream)
    (StreamFilter, RepoFilter, GroupingFilter, TagsFilter)
    (DropFilter): New classes for processing repo stats.

--- helpers.py
+++ helpers.py
@@ -1,129 +1,255 @@
 import datetime
 
+from mercurial.localrepo import localrepository
+
 """
-Generators, filters and output methods for hgstats.
+Streams, filters and output methods for hgstats.
 
 Copyright (C) 2009 Dmitry Dzhus <dima@sphinx.net.ru>
 
 This code is subject to GNU General Public License version 2
 """
 
-## Statistics sources
-##
-## INPUT: Mercurial localrepo object
-##
-## OUTPUT: Iterable of 3-tuples ``(dt, n, ctx)``, where ``dt`` is a
-## change datetime, ``n`` is undefined and ``ctx`` is a change
-## context; or a generator
-
-def repo_revcount(repo):
-    """
-    Generates tuples of revision counts with corresponding dates of
-    change.
-    """
-    data = []
-    for n in xrange(len(repo)):
-        yield (datetime.datetime.fromtimestamp(repo[n].date()[0]),
-               n + 1,
-               repo[n])
-
-def list_to_source(data):
-    """
-    >>> g = list_to_source([1, 2, 3])
-    >>> g.next()
-    1
-    """
-    def source():
-        for x in data:
-            yield x
-    return source()
-
-## Filters
-##
-## INPUT: Valid source output
-## 
-## OUTPUT: Iterable of iterables which have string representation
-
-def not_too_old(up_to, delta):
-    """Make *filter* which will return True only for chunks which
-    are no earlier than `up_to-date_delta`"""
-    def test(chunk):
-        return chunk[0] > (up_to - delta)
-    return test
-
-def snap_date(date):
-    """Snap to beginning of day."""
-    return datetime.datetime(*date.timetuple()[:3]) # It's a lion!
-
-def group_by_date_filter(data,
-                         datemin=datetime.datetime(2007, 1, 1),
-                         datemax = datetime.datetime.now(),
-                         resolution=1, relax_days=7):
-    """
-    Group data by equal timespans, as specified with `resolution` (in
-    days).
-
-    `relax_days` is a beat relaxation time (in days).
-    """
-    cur_date = snap_date(datemin)
-    delta = datetime.timedelta(resolution)
-    relax_period = datetime.timedelta(relax_days)
-
-    # We assume that data contains at least one chunk
-    chunk = data.next()
-    group = []
-    while cur_date < datemax:
-        # Drop old chunks
-        # Collect new chunks
-        while chunk[0] < cur_date:
-            group.append(chunk)
-            try:
-                chunk = data.next()
-            except StopIteration:
-                break
-        group = filter(not_too_old(cur_date, relax_period), group)
-            
-        # Upcoming chunk occured, flushing collected group
-        # This function should accept arbitary spangrouping function
-        yield [cur_date.strftime('%s'), len(group)]
-
-        cur_date += delta
-                
-## Statistics storage
-##
-## INPUT: Valid filter output
-## 
-## OUTPUT: Undefined
-
-def make_stats_line(data_chunk, line_sep='\n', field_sep=' '):
-    """Prepare writable line from `data_chunk`."""
-    return field_sep.join(map(str, data_chunk)) + line_sep
+## Various functions
 
 def get_repo_name(repo):
     from os.path import split
     return split(split(repo.path)[0])[1]
 
-def write_stats_file(repo, data):
-    file_name = 'stats-' + get_repo_name(repo)
+## Exceptions
+
+class IncompatibleFilter(BaseException):
+    pass
+
+class UnsyncedStreams(BaseException):
+    pass
+
+## Statistics items
+
+class StatItem():
+    """
+    Represents a single item in repository statistics.
+    """
+    def __init__(self, x, y, x_label=None, y_label=None):
+        """
+        `x` and `y` are values used for X and Y axis, respectively,
+        whereas `x_label` and `y_label` will be used to place labels
+        for the item.
+
+        If `y` is not specified, it's set to `x`. If `x_label` and
+        `y_label` are not specified, they're set to string
+        representations of `x` and `y`, repsectively.
+        """
+        self.x = x
+        self.y = y
+        self.x_label = x_label or str(x)
+        self.y_label = y_label or str(y)
+
+    def __repr__(self):
+        return '(%d,%d)' % (self.x, self.y)
+
+class CtxStatItem(StatItem):
+    """
+    Wraps change context in a `StatItem` instance.
+    """
+    def __init__(self, ctx, *args, **kwargs):
+        StatItem.__init__(self, *args, **kwargs)
+        self.ctx = ctx
+        self.datetime = datetime.datetime.fromtimestamp(ctx.date()[0])
+
+    def __repr__(self):
+        return '%s (%d,%d)' % (str(self.ctx), self.x, self.y)
+
+## Streams form sequences of StatItems
+
+class StatStream():
+    """
+    Wraps containters.
+    """
+    def __init__(self, stream):
+        """
+        `stream` must be an iterable of `StatItem` objects.
+        """
+        self.stream = stream
+
+    def __getitem__(self, i):
+        return self.stream[i]
+
+    def __iter__(self):
+        return iter(self.stream)
+
+class RepoStream(StatStream):
+    """
+    Stream of change contexts in a repository, represented by
+    `CtxStatItem` instances.
+    """
+    def __init__(self, stream):
+        """
+        Constructs new RepoStream instance by converting an existing
+        Mercurial repository or binding `CtxStatItem` objects produced
+        by filter.
+
+        `repo` must be a `mercurial.localrepo.localrepository`
+        instance or an iterable of `CtxStatItem` objects.
+
+        In the former case, iterating over the created instance will
+        give `CtxStatItem` objects with changeset dates as ``x`` and
+        revision numbers as ``y``.
+        """
+        StatStream.__init__(self, stream)
+
+    def __iter__(self):
+        if isinstance(self.stream, localrepository):
+            for rev in self.stream:
+                ctx = self.stream[rev]
+                yield CtxStatItem(ctx, x=ctx.date()[0], y=ctx.rev())
+        else:
+            for item in self.stream:
+                # assert(isinstance(item, CtxStatItem))
+                yield item
+
+    def __len__(self):
+        return len(self.stream)
+
+## Filters transform streams, producing another streams. Every filter
+## must return a StatStream instance upon calling.
+
+class StreamFilter():
+    def __init__(self, stream):
+        # check that we apply filter to stream
+        if not isinstance(stream, StatStream):
+            raise IncompatibleFilter('%s may be applied to StatStream only' % self.__class__)
+        self.stream = stream
+
+class RepoFilter(StreamFilter):
+    def __init__(self, repo):
+        StreamFilter.__init__(self, repo)
+        # check that we may rely on ctx information in class methods
+        if not isinstance(repo, RepoStream):
+            raise IncompatibleFilter('%s may be applied to RepoStream only' % self.__class__)
+
+class GroupingFilter(RepoFilter):
+    "Accepts RepoStream, produces StatStream."
+    def __init__(self, repo, resolution=7, relax_days=7):
+        """
+        Constructs a new GroupedStream instance which group
+        `CtxStatItem` objects from `repo` by equal timespans, as
+        specified with `resolution` (in days).
+
+        `relax_days` is a beat relaxation time (in days).
+        `CtxStatItem` will be included in a group if its datetime is
+        not earlier that `relax_days` before the end of a time frame
+        for that group.
+
+        ---[-------resolution=10-------]---
+        ---[-----------[-relax_days=5-]]---
+        ---[--o---o-----x--xxx--x------]---
+
+        Here only ``x`` commits will be included in the group."""
+        RepoFilter.__init__(self, repo)
+        self.resolution = resolution
+        self.relax_days = relax_days
+
+    def __iter__(self):
+        def not_too_old(up_to, delta):
+            """Make *filter* which will return True only for chunks which
+            are no earlier than `up_to-date_delta`"""
+            def test(chunk):
+                return chunk.datetime > (up_to - delta)
+            return test
+
+        def snap_date(date):
+            """Snap to beginning of day."""
+            return datetime.datetime(*date.timetuple()[:3]) # It's a lion!
+
+        data = iter(self.stream)
+
+        # We assume that data contains at least one item
+        item = data.next()
+
+        cur_date = snap_date(item.datetime)
+        datemax = datetime.datetime.now()
+        delta = datetime.timedelta(self.resolution)
+        relax_period = datetime.timedelta(self.relax_days)
+
+        group = []
+        while cur_date < datemax:
+            # Collect new items
+            while item.datetime < cur_date:
+                group.append(item)
+                try:
+                    item = data.next()
+                # No more items in the stream
+                except StopIteration:
+                    break
+            # Drop old items
+            group = filter(not_too_old(cur_date, relax_period), group)
+
+            # Upcoming chunk occured, flushing collected group
+            # This function should accept arbitary spangrouping function
+            yield StatItem(x=cur_date.strftime('%s'), y=len(group))
+
+            cur_date += delta
+
+class TagsFilter(RepoFilter):
+    "Filters out non-tagged changesets."
+    def __iter__(self):
+        for item in self.stream:
+            if item.ctx.tags():
+                yield item
+
+class DropFilter(StreamFilter):
+    def __init__(self, stream, target_stream):
+        """
+        Contruct a new DropFilter instance which will make ``y``
+        attributes of ``StatItem`` objects in `stream` equal to those
+        of items in `target_stream` which have the same ``x``
+        attribute value.
+
+                               o
+                            .    |
+                          .. .   |
+                 o    .x..    . .x
+                 |  .. |       .  ....
+                 x..   o
+                ..
+        ..  ....
+          ..
+
+        . - `stream` items
+        o - `target_stream` items
+        x - items produced by this filter
+        """
+        StreamFilter.__init__(self, stream)
+        self.target_stream = target_stream
+
+    def __iter__(self):
+        for item in self.stream:
+            target_data = iter(self.target_stream)
+            target_item = target_data.next()
+            while not item.x == target_item.x:
+                try:
+                    target_item = target_data.next()
+                except StopIteration:
+                    raise UnsyncedStreams('%s does not contain item with x=%d'\
+                                          % (self.target_stream, item.x))
+            yield StatItem(item.x, target_item.y,
+                           item.x_label, item.y_label)
+
+## Output routines
+
+def make_stats_line(item, line_sep='\n', field_sep=' '):
+    """Prepare writable line from `StatsItem` instance."""
+    return field_sep.join([item.x_label, item.y_label]) + line_sep
+
+def write_stats(stream,file_name='stats'):
     stats_file = open(file_name, 'w')
     # I don't use writelines intentionally
-    for chunk in data:
+    for chunk in stream:
         stats_file.write(make_stats_line(chunk))
     stats_file.close()
 
-def print_stats(repo, data, line_sep='\n'):
-    for chunk in data:
-        print make_stats_line(chunk),
-
-# Dictionary of named data sources
-sources_dict = {None: repo_revcount}
-
-# Dictionary of named data filters
-filters_dict = {None: None,
-                'date': group_by_date_filter}
-
-# Dictionary of data output methods
-output_dict = {None: write_stats_file,
-              'google': None,
-              'file': write_stats_file,
-              'print': print_stats}
+def print_stats(repo, stream, line_sep='\n'):
+    print "# Stats for %s " % repo.path
+    for item in stream:
+        print make_stats_line(item),
--- hgstats.py
+++ hgstats.py
@@ -34,58 +34,26 @@ import getopt
 import datetime
 
 from mercurial import hg, ui
-from mercurial.repo import RepoError
+from mercurial.error import RepoError
 from mercurial.i18n import _
 
-from helpers import sources_dict, filters_dict, output_dict
+from helpers import RepoStream, GroupingFilter, print_stats
 
 def print_usage():
     print _("Usage: ./foostats.py [PATH1 [PATH2 [..]]]")
     
-def make_repo_stats(repo, stat_source, stat_filter=None):
-    """
-    Gather statistics for `repo` with `generator`, apply `filter` to
-    data and return result.
-    """
-    stats = stat_source(repo)
-    if stat_filter:
-       stats = stat_filter(stats) 
-    return stats
+def make_repo_stats(repo):
+    stream = (RepoStream(repo))
+    return stream
 
-def process_repos(repo_list, stat_generator, stat_filter, output_method):
+def process_repos(repo_list):
     """
     Given a list of repos, gather stats for each of them and output
     data using `output_method`.
     """
     for repo in repo_list:
-        output_method(repo, make_repo_stats(repo, stat_generator, stat_filter))
-
-def make_options(cl):
-    """
-    Given command line options list, return 4 values: a list of paths
-    to repositories to process and names of stats generator, filter
-    and output method.
-    """
-    try:
-        options, arguments = getopt.gnu_getopt(cl, 'm:f:o:',
-                                               ['method=', 'filter=', 'output='])
-    except getopt.GetoptError, err:
-        print str(err)
-        print_usage()
-        sys.exit(1)
-
-    source_name = filter_name = output_name = None
-
-    for o, v in options:
-        if o in ['-m', '--method']:
-            source_name = v
-        elif o in ['-f', '--filter']:
-            filter_name = v
-        elif o in ['-o', '--output']:
-            output_name = v
-        else:
-            assert False
-    return arguments, source_name, filter_name, output_name
+        print_stats(repo, make_repo_stats(repo))
+        print "\n\n",
 
 def get_repo(repo_path):
     """Get repository by its URL or False if it doesn't exist."""
@@ -95,15 +63,7 @@ def get_repo(repo_path):
         repo = False
     return repo
 
-# Simple version
-def impl_by_name(name, impl_dict):
-    # Perhaps possible exception should be specifically handled
-    return impl_dict[name]
-
-def run_stats(path_list, s_name, f_name, o_name):
-    # Select source, filter and output by their names
-    s, f, o = [impl_by_name(n, d) for n, d in (s_name, sources_dict), (f_name, filters_dict), (o_name, output_dict)]
-    
+def run_stats(path_list):
     def ignore_bad(path):
         repo = get_repo(path)
         if not repo:
@@ -114,9 +74,9 @@ def run_stats(path_list, s_name, f_name, o_name):
             return False
         else:
             return repo
-
+    # We process only good repositories
     repo_list = filter(None, map(ignore_bad, path_list))
-    process_repos(repo_list, s, f, o)
+    process_repos(repo_list)
 
 if __name__ == '__main__':
-    run_stats(*make_options(sys.argv[1:]))
+    run_stats(sys.argv[1:])
