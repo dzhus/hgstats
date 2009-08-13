0f102b5a7626910cbc6bee7f633f840d66bf0919
Dmitry Dzhus <dima@sphinx.net.ru>
2009-08-13 14:37:12 +0400

    (DiffstatFilter): Now diffs against parent nodes, not previous item in the stream, because diffing between stream items is wrong when they are from different branches and leads to awkward results.

--- helpers.py
+++ helpers.py
@@ -295,22 +295,20 @@ class DiffstatFilter(RepoFilter, StatStream):
     """
     Sets diffstat results as ``y`` values.
     """
-    def __init__(self, stream, show_delta=False, include_initial=True):
+    def __init__(self, stream, show_delta=False):
         """
         Construct new `DiffstatFilter` instance for `stream`.
 
         If `show_delta` if False, ``y`` of produced items is set to
-        sum of lines added and lines removed since previous item.
+        sum of lines added and lines removed since parent revision.
         Otherwise, difference between these two values is used
         instead.
+
+        Merge changesets are not included.
 
         For example, if 15 lines were added and 6 removed between
         items, 15+6=21 will be used in case `show_delta` is True and
         15-6=9 otherwise.
-
-        For the first item of `stream`, nothing is produced unless
-        it's context has revision 0 and `include_initial` is True, in
-        which case the difference between 0 and null revision is used.
 
         If `AccFilter` is applied after this one when `show_delta` is
         True, a stream of total repository sizes (in lines) will be
@@ -322,25 +320,15 @@ class DiffstatFilter(RepoFilter, StatStream):
         else:
             self.delta_function = lambda t: t[1] + t[2]
         self.show_delta = show_delta
-        self.include_initial = include_initial
 
     def __iter__(self):
-        # We can't assume that the whole changeset history is present
-        # in the stream, thus we keep track of what ctx was yielded
-        # last time, so we can diff current item against it.
-        data = iter(self.stream)
-        first = data.next()
-        if first.ctx.rev() == 0 and self.include_initial:
-            prev_node = first.ctx.p1().node()
-        else:
-            prev_node = first.ctx.node()
-        for item in data:
+        for item in self.stream:
             ctx = item.ctx
-            p = ''.join(patch.diff(ctx._repo, prev_node, ctx.node()))
-            lines = p.split('\n')
-            stats = sum(map(self.delta_function, patch.diffstatdata(lines)))
-            yield item.child(x=ctx.date()[0], y=stats)
-            prev_node = ctx.node()
+            if len(ctx.parents()) < 2:
+                p = ''.join(patch.diff(ctx._repo, ctx.p1().node(), ctx.node()))
+                lines = p.split('\n')
+                stats = sum(map(self.delta_function, patch.diffstatdata(lines)))
+                yield item.child(x=ctx.date()[0], y=stats)
 
 class DropFilter(StreamFilter, StatStream):
     """
