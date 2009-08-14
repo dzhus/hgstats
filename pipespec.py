ce3b8bbf78fdc16adb345358972c8fd25ed6d987
Dmitry Dzhus <dima@sphinx.net.ru>
2009-08-14 17:05:05 +0400

    Shlex-based pipespec parser.

--- pipespec.py
+++ pipespec.py
@@ -4,7 +4,7 @@ Description
 
 This module provides `parse_pipespec` function which translates
 textual description of filter sequence from to function which applies
-descibed filters to `RepoStream` instance.
+descibed filters to `mercurial.localrepository` objects.
 
 Author and licensing
 ====================
@@ -15,6 +15,9 @@ This code is subject to GNU GPL version 2 license, as can be read on
 http://www.gnu.org/licenses/gpl-2.0.html.
 """
 
+import shlex
+
+from helpers import RepoStream
 from helpers import GroupingFilter, AccFilter, TagsFilter, DiffstatFilter
 
 symtable = {
@@ -24,11 +27,103 @@ symtable = {
     'TagsFilter': TagsFilter
     }
 
-def parse_pipespec(pipespec):
-    chunks = pipespec.split(':', 1)
+class Error(Exception):
+    pass
+
+class UnknownFilter(Error):
+    pass
+
+class BadArgument(Error):
+    pass
+
+class UnexpectedEnd(Error):
+    pass
+
+def _read_args(shlex_obj):
+    """
+    Read pipespec filter arguments from `shlex_obj` tokens.
+
+    >>> _read_args(shlex.shlex('(1, 2, True)'))
+    [1, 2, True]
+
+    Only integer numbers, True and False are allowed.
+
+    >>> _read_args(shlex.shlex('(\"Foo\")'))
+    Traceback (most recent call last):
+      ...
+    BadArgument: \"Foo\"
+
+    >>> _read_args(shlex.shlex('(1,'))
+    Traceback (most recent call last):
+      ...
+    UnexpectedEnd
     
-    if len(chunks) == 1:
-        return lambda s: symtable[chunks[0]](s)
+    >>> _read_args(shlex.shlex('()'))
+    []
+    >>> _read_args(shlex.shlex(''))
+    []
+    >>> _read_args(shlex.shlex('not_args'))
+    []
+    """
+    token = shlex_obj.get_token()
+    args = []
+    if token == '(':
+        for token in shlex_obj:
+            if token == ',':
+                continue
+            elif token == ')':
+                return args
+            # Numbers
+            elif unicode(token).isdecimal():
+                args += [int(token)]
+            # Booleans
+            elif token == 'True':
+                args += [True]
+            elif token == 'False':
+                args += [False]
+            else:
+                raise BadArgument(token)
     else:
-        return lambda s: parse_pipespec(chunks[1])(parse_pipespec(chunks[0])(s))
-    
+        # No args in shlex_obj
+        shlex_obj.push_token(token)
+        return args
+    raise UnexpectedEnd
+
+def _read_filter(shlex_obj):
+    """
+    Read next filter specifier (with arguments) from `shlex_obj` tokens
+    and return function.
+    """
+    filter_name = shlex_obj.get_token()
+    if filter_name == '':
+        return False
+    elif symtable.has_key(filter_name):
+        args = _read_args(shlex_obj)
+        return lambda s: symtable[filter_name](s, *args)
+    else:
+        raise UnknownFilter(filter_name)
+
+def _read_pipespec(shlex_obj, filters):
+    """
+    Read next filter description from `shlex_obj` and compose it with
+    `filters`.
+    """
+    cur_filter = _read_filter(shlex_obj)
+    if cur_filter:
+        return _read_pipespec(shlex_obj, lambda s: cur_filter(filters(s)))
+    else:
+        return filters
+
+def parse_pipespec(pipespec):
+    """
+    Return a function, which performs a sequence of filter
+    applications as described in `pipespec` string.
+    """
+    shlex_obj = shlex.shlex(pipespec)
+    # We just ignore all colons
+    shlex_obj.whitespace += ':'
+    return _read_pipespec(shlex_obj, RepoStream)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
